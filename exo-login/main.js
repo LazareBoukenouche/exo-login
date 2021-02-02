8ab39d6718b37c30d2099414b30461ce7fa3cc80
LazareBoukenouche <lazare.boukenouche@gmail.com>
2021-02-02 09:04:11 +0100

    form validation

--- exo-login/main.js
+++ exo-login/main.js
@@ -1,6 +1,35 @@
-document.querySelector("form").addEventListener("submit", () => {
-  // avant d'envoyer les données au serveur, on peut faire une étape de validation
+// document.querySelector("form").addEventListener("submit", (event) => {
+//   let isValid = true;
 
-  // on n'envoie pas vraiment les données pour l'exercice
-  document.querySelector("main").innerHTML = "Vous êtes connecté :)";
-});
+//   let username = document.querySelector('#username');
+//   let password = document.querySelector('#password');
+  
+//   if (username.value === "") {
+//     isValid = false;
+//     event.preventDefault();
+//     // afficher une erreur
+//   }
+//   if (password.value === "") {
+//     // afficher une erreur
+//     isValid = false;
+//     event.preventDefault();
+//   }
+
+//   if(isValid) {
+//     // on n'envoie pas vraiment les données pour l'exercice
+//     document.querySelector("main").innerHTML = "Vous êtes connecté :)";
+//   }
+  
+
+  
+// });
+
+// function displayPassword() {
+//   let input = document.querySelector('#toggle-password-visibility');
+//   if (input.checked) {
+//       password.type = "text";
+//   }
+//   else {
+//       password.type = "password";
+//   }
+// }
